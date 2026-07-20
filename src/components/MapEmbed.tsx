144ce6a84c5217a6f27fa055de3057741c7136f2
Charanbalaji-Sparkout <charanmohan0508@gmail.com>
2026-07-20 21:09:21 +0530

    chore: correct map coordinates to exactly match listing location pin at 11.3412408,77.7178139

--- src/components/MapEmbed.tsx
+++ src/components/MapEmbed.tsx
@@ -2,7 +2,7 @@ export default function MapEmbed() {
   return (
     <div className="relative w-full h-[400px] rounded-2xl overflow-hidden shadow-lg border border-gray-100 group">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3911.913437525381!2d77.71726671083944!3d11.341246088800918!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba96f8ef1a473e7%3A0xca26eb5dd11fae8e!2sSree+Balaji+Advertising!5e0!3m2!1sen!2sin!4v1721487823528!5m2!1sen!2sin"
+        src="https://maps.google.com/maps?q=11.3412408,77.7178139(Sree%20Balaji%20Advertising)&t=&z=19&ie=UTF8&iwloc=&output=embed"
         width="100%"
         height="100%"
         style={{ border: 0 }}
